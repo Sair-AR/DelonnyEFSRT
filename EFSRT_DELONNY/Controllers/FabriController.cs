e07d272c862fe8f02de1d8de95c4c915173c8441
20Fabri <kajadered@gmail.com>
2025-05-26 16:52:50 -0500

    prueba creando un controller

---
+++ EFSRT_DELONNY/Controllers/FabriController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFSRT_DELONNY.Controllers
+{
+    public class FabriController : Controller
+    {
+        // GET: Fabri
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
